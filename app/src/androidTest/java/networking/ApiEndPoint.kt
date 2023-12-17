9e9e3a55a4fa2bba3b65e8c3b32802c142649606
cacalawak <risayusnita02@gmail.com>
2023-12-17 22:38:17 +0700

    1. membuat folder networking diisi dengan nama class AiEndPoint yang dimana berisi link untuk menampilkan data tersebut. 2. membuat folder utils yang didalamnya ada BaseLayoutMargin, BottomBarBehavior, LayoutMarginDecoration, MarginDelegate, OnClickLayoutMarginItemListener, Tools.

---
+++ app/src/androidTest/java/networking/ApiEndPoint.kt
@@ -0,0 +1,11 @@
+package networking
+
+public class ApiEndPoint {
+    public static String BASEURL = "https://mangamint.glitch.me/api/manga/page/";
+    public static String ALSOURL = "https://mangamint.glitch.me/api/recomended";
+    public static String GENREURL = "https://mangamint.glitch.me/api/genres";
+    public static String DETAILMANGAURL = "https://mangamint.glitch.me/api/manga/detail/{endpoint}";
+    public static String CHAPTERURL = "https://mangamint.glitch.me/api/chapter/{chapter_endpoint}";
+    public static String GENREURL = "https://mangamint.glitch.me/api/genres/{endpoint}";
+
+}
